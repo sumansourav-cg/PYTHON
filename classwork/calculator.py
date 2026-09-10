b78bcc9c11edfa483421d4817747d90eab4cf249
sumansourav-cg <sumansourav.cg@gmail.com>
2026-09-10 09:44:05 +0530

    folder added

---
+++ classwork/calculator.py
@@ -0,0 +1,20 @@
+print("1 for addition,2 for subtraction,3 for multiplication,4 for remainder,5 for division")
+
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+c=int(input("which operation do you want to perform"))
+if c==1 or c==2 or c==3 or c==4 or c==5 :
+
+ if c==1 :
+
+    print(a+b)
+ elif c==2:
+    print(a-b)
+ elif c==3:
+    print(a*b) 
+elif c==4 :
+    print(a%b)
+elif c==5 :
+    print(a/b)
+else:
+    print("beta tmse na ho payega")               
---
+++ classwork/has.py
@@ -0,0 +1,7 @@
+a=input("Enter if he has id(yes/no) : ").strip().lower() 
+if a=="no":
+    print("get lost")
+elif a=="yes":
+    print("Welcome!!!")
+else:
+    print("enter a valid value")
